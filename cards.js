327725e809363d84c78d98df40cc49f669ae8232
SeldomseenSchweig <jamesschweig@gmail.com>
2022-09-06 11:34:08 -0700

    done

--- cards.js
+++ cards.js
@@ -1,39 +1,83 @@
+const url = 'https://deckofcardsapi.com/api/deck/'
+
+
+1.
+
+
+async function draw1(){
+    let res = await axios.get(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+    id = res.data.deck_id
+    let card =  await axios.get(`${url}${id}/draw/?count=1`)
+    // console.log(card[0].value + "of" + card[0].suit)
+    value = card.data.cards[0].value.charAt(0) + card.data.cards[0].value.substring(1).toLowerCase();
+    suit = card.data.cards[0].suit.charAt(0) + card.data.cards[0].suit.substring(1).toLowerCase();
+    console.log(value + " of " + suit)
+
+}
+
+2.
+
+
+async function draw2(){
+    let res = await axios.get(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+    id = res.data.deck_id
+    let card1 =   axios.get(`${url}${id}/draw/?count=1`)
+    let card2 =  axios.get(`${url}${id}/draw/?count=1`)
+
+    let newCard1 = await card1
+    let newCard2 = await card2
+
+
+    value = newCard1.data.cards[0].value.charAt(0) + newCard1.data.cards[0].value.substring(1).toLowerCase();
+    suit = newCard1.data.cards[0].suit.charAt(0) + newCard1.data.cards[0].suit.substring(1).toLowerCase();
+    value2 = newCard2.data.cards[0].value.charAt(0) + newCard2.data.cards[0].value.substring(1).toLowerCase();
+    suit2 = newCard2.data.cards[0].suit.charAt(0) + newCard2.data.cards[0].suit.substring(1).toLowerCase();
+    console.log(value + " of " + suit + " and the " + value2 + " of " + suit2 )
+
+}
 
 
 
 
-class Deck{
-    constructor(){
-        async function deck(){
-            let res = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')   
+
+
+
+
+
+3.
+  
+        $('#deck').append("<img src='https://cdn.pixabay.com/photo/2012/05/07/18/52/card-game-48980_960_720.png'> </img>")
+        let id
+        deck()
+        
+        
+         $('#draw').click(draw)
+
+
+
+
+
+
+    async function deck(){
+        let res = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        id = res.data.deck_id
+            
         }
-        let res = deck()
-        this.id = res.data.deckid
-         
-        this.url = 'https://deckofcardsapi.com/api/deck/'
-        }
 
-    async shuffle(){
-        await axios(`${url}/${this.id}/shuffle`)
-        $('#deck').append("<img src='https://pixabay.com/get/g5f22bef9ad56027f6bfd55f1c71854b335ca763261e67023414c5a6b093b8b326c2d71c61dabdcd66b3cfb89319aec04_1280.png'> </img>")
-
-    }
-    async draw(){
-       let card =  await axios.get(`${url}${this.id}/draw/?count=1`)
+    async function draw(){
+       let card =  await axios.get(`${url}${id}/draw/?count=1`)
         if (card.data.remaining !== 0){
             $('#deck').html(`<img src=${card.data.cards[0].image}> </img>`)
         }else{
             alert('Deck is Empty!')
             $("button").prop("disabled",true)
+  
         }
         }
 
-    async newDeck(){
-        this.deck()
-        $('#deck').append("<img src='https://pixabay.com/get/g5f22bef9ad56027f6bfd55f1c71854b335ca763261e67023414c5a6b093b8b326c2d71c61dabdcd66b3cfb89319aec04_1280.png'> </img>")
-    }
 
-    }
+
+    
 
 
 
